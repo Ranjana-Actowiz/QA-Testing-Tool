b754882a3a0ea12fd9d0b67ed27951736b789503
Ranjana-Actowiz <Ranjana.Yadav@actowiz.co.in>
2026-07-03 12:14:41 +0530

    chore: add console log for testing in Footer component

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,4 +1,5 @@
  const  Footer = () => {
+    console.log("testing ")
     return (
         <footer
             className="text-right text-sm text-white/60 px-4 sm:px-6 lg:px-8 py-4 mt-auto bg-violet-500/70"
